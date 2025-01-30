444f3b402fa3f8ccc06492bc21b63932cebc8367
Brian D. Caruso <bdc34@cornell.edu>
2025-01-30 12:58:06 -0500

    Typos

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from status.factory import create_web_app
+import os
 
 app = create_web_app()
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host="0.0.0.0" port=init(os.environ.get("PORT",8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT",8080)))
